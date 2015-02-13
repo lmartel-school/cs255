9822fbf270845f6a488d453e1861ddaecaf2c7a7
Leo Martel <lmartel@github>
2015-02-12 19:34:57 -0800

    new tests

--- proj1/test-password-manager.js
+++ proj1/test-password-manager.js
@@ -15,7 +15,7 @@ var keychain = password_manager.keychain();
 console.log("Initializing a toy password store");
 keychain.init(password);
 
-var kvs = { "service1": "value1", 
+var kvs = { "service1": "value1",
             "service2": "value2",
             "service3": "value3" };
 
@@ -43,7 +43,7 @@ var cksum = data[1];
 
 console.log("Loading database");
 var new_keychain = password_manager.keychain();
-new_keychain.load(password, contents, cksum);
+assert(new_keychain.load(password, contents, cksum) === true);
 
 console.log("Checking contents of new database");
 for (var k in kvs) {
@@ -51,3 +51,55 @@ for (var k in kvs) {
 }
 
 console.log("All tests passed!");
+
+console.log("Time for more tests!");
+
+new_keychain = password_manager.keychain();
+assert(new_keychain.load("not the password", contents) === false);
+
+var not_ready = password_manager.keychain();
+assert(not_ready.dump() === null);
+should_throw(not_ready.set, "foo", "bar");
+should_throw(not_ready.get, "foo");
+should_throw(not_ready.remove, "foo");
+
+var empty = password_manager.keychain();
+empty.init("nofriendsQQ");
+assert(empty.get("foo") === null);
+assert(empty.remove("foo") === false);
+
+// should throw when corrupt
+var tamper = JSON.parse(contents);
+tamper.passwords["foo"] = "bar";
+
+var sneaky = password_manager.keychain();
+
+should_throw(sneaky.load, password, JSON.stringify(tamper), cksum);
+should_throw(sneaky.set, "foo", "bar");
+
+// Even if loaded without checksum, tampered entries should not be retrievable
+var tampered_key;
+for (k in tamper.passwords){
+  tampered_key = k;
+  tamper.passwords[k][0]++;
+  break;
+}
+sneaky.load(password, JSON.stringify(tamper));
+should_throw(sneaky.get, "foo");
+should_throw(sneaky.get, tampered_key);
+
+
+console.log("All bonus tests passed.");
+
+/* Helpers */
+
+function should_throw(fn /*, ... */){
+  var args = Array.prototype.slice.call(arguments, should_throw.length);
+  var threw = false;
+  try {
+    fn.apply(args);
+  } catch(e) {
+    threw = true;
+  }
+  assert(threw);
+}
